d2c87b3d8c9728ccbd673cd52d2b6bb66221c293
GSerejo <gabrielserejo21@gmail.com>
2026-03-02 20:21:36 -0300

    Adciona arquivo .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,22 @@
 function Home() {
-   
-    return (
-        <>
-            <h1>Vida!</h1>
-            <h2>Eu te mandei isso aqui so para dizer o quanto eu te amo! hihi</h2>
-            
-        <iframe 
-            data-testid="embed-iframe" 
-            style={{"border-radius":"12px"}} 
-            src="https://open.spotify.com/embed/track/4WbkVm4eW824meQyaf4qfv?utm_source=generator" 
-            width="100%" 
-            height="352" 
-            frameBorder="0" 
-            allowfullscreen="" 
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-            loading="lazy">
-        </iframe>
-        </>
-    );
-    
-    
+  return (
+    <>
+      <h1>Vida!</h1>
+      <h2>Eu te mandei isso aqui so para dizer o quanto eu te amo! hihi</h2>
+
+      <iframe
+        data-testid="embed-iframe"
+        style={{ "border-radius": "12px" }}
+        src="https://open.spotify.com/embed/track/4WbkVm4eW824meQyaf4qfv?utm_source=generator"
+        width="100%"
+        height="352"
+        frameBorder="0"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+    </>
+  );
 }
 
 export default Home;
